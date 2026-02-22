31ca39e18e056c892677e00e52e1afcc9d17345c
Appl <appl@Appls-MacBook-Air.local>
2026-02-22 23:02:52 +0530

    Initial commit

---
+++ app/aboutus/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>
+      About us Page
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ app/services/laptop-repair/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LaptopRepair = () => {
+  return (
+    <div>
+      Laptop Repairing
+    </div>
+  )
+}
+
+export default LaptopRepair
