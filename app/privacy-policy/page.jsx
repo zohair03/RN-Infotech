ed446081ba3f40d3d0cc17f1583100140e495f5d
Zohair Ahmed <zohairrahmed03@gmail.com>
2026-04-14 10:41:08 +0530

    privacy-policy burg fix

--- app/privacy-policy/page.jsx
+++ app/privacy-policy/page.jsx
@@ -12,7 +12,7 @@ const PrivacyPolicy = () => {
       <SubpageHero
         title="Privacy Policy"
         breadcrumb="Privacy Policy"
-        image="/images/rn-infotech-19.webp"
+        image="/images/rn-infotech-09.webp"
         cta1="Call Us Now"
         cta2="Contact Us"
         href2="/contact"
